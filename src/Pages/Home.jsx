9acfd738a8ed69c3d37209736cc11e3d5be2a5f4
Trainings <Trainings@datanova.co.in>
2024-03-04 18:33:13 +0530

    home fixed css working

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,43 @@
-export const Home =() => {
-    return <h1> Hello Home Page </h1>;
+export const Home = () => {
+  return (
+    <>
+      <main>
+        <section className="section-hero">
+          <div className="container grid grid-two-cols">
+            <div className="hero-content">
+              <p>Hello This is vishal</p>
+              <h1>Welcome to my website</h1>
+              <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod
+                ducimus aut a mollitia reiciendis dolore sapiente accusamus
+                dicta impedit quas, assumenda iure quaerat dolores itaque,
+                fugiat molestias libero, aperiam quae iusto quo. Qui ipsum
+                possimus eaque, dolores, placeat beatae nihil ad ipsa obcaecati
+                veritatis impedit architecto quam excepturi maiores saepe sint
+                nesciunt repellendus! Nemo qui dolorem sint dolor perferendis
+                ducimus debitis cum sed eligendi minima dolorum nam tempore
+                omnis praesentium cupiditate iusto, error voluptas est
+                reprehenderit recusandae? Molestiae ipsum quam libero voluptate
+                inventore sapiente illum quisquam? Dignissimos commodi at cumque
+                id repellat repellendus possimus distinctio delectus quaerat
+                aspernatur. Accusantium, saepe!
+              </p>
+              <div className="btn btn-group">
+                <a className='home-btn' href="/contact">
+                  <button className="btn">Connect now</button>
+                </a>
+                <a className='home-btn' href="/services">
+                  <button className="btn">Learn More</button>
+                </a>
+              </div>
+            </div>
+{/* Images */}
+            <div className="hero-image">
+                <img src="/Image/TR00081.jpg" alt="Home-pic" width={400} height={400}/>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 };
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,73 +1,78 @@
 import { useState } from "react";
 
-export const Login =() => {
-   const [user,setUser]=useState({
-    email:"",
-    password:"",
-   });
-   const handleSubmit = (e) =>{
+export const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log(user);
-   }
-   const handleInput =(e) =>{
+  };
+  const handleInput = (e) => {
     // console.log(e);
     let name = e.target.name;
     let value = e.target.value;
     console.log(value);
     setUser({
-        ...user,
-        [name]:value
-        
-    })
-   }
-    return <>
-    <section>
+      ...user,
+      [name]: value,
+    });
+  };
+  return (
+    <>
+      <section>
         <main>
-            <div className="section-login">
-                <div className="container grid grid-two-cols">
-                    <div className="login-image">
-                        <img src="/Image/login.png" alt="My pic"  /* for your pic TR00081.jpg*/
-                        width={500}
-                        height={500}
-                        />
-                    </div>
-                    <div className="login-form">
-                        <h1 className="main-heading mb-3">login form</h1>
-                        <br/>
+          <div className="section-login">
+            <div className="container grid grid-two-cols">
+              <div className="login-image">
+                <img
+                  src="/Image/login.png"
+                  alt="My pic" /* for your pic TR00081.jpg*/
+                  width={500}
+                  height={500}
+                />
+              </div>
+              <div className="login-form">
+                <h1 className="main-heading mb-3">login form</h1>
+                <br />
 
-                        <form  onSubmit={handleSubmit}>
-                            <div>
-                                <label htmlFor="email">email</label>
-                                <input 
-                                type="email" 
-                                name="email"
-                                placeholder="Enter your email"
-                                id="email"
-                                required
-                                autoComplete="off"
-                                value={user.email}
-                                onChange={handleInput}
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor="password">password</label>
-                                <input 
-                                type="password" 
-                                name="password"
-                                placeholder="Enter your password"
-                                id="password"
-                                required
-                                autoComplete="off"
-                                value={user.password}
-                                onChange={handleInput}
-                                />
-                            </div>
-                            <button type="submit"className="btn btn-submit">Login Now</button>
-                        </form>
-                    </div>
-                </div>
+                <form onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Enter your email"
+                      id="email"
+                      required
+                      autoComplete="off"
+                      value={user.email}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Enter your password"
+                      id="password"
+                      required
+                      autoComplete="off"
+                      value={user.password}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <button type="submit" className="btn btn-submit">
+                    Login Now
+                  </button>
+                </form>
+              </div>
             </div>
+          </div>
         </main>
-    </section>
-    </>;
+      </section>
+    </>
+  );
 };
